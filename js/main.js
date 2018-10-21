d8907657bea9d5dded40ba0e3e0a51b8fcff5ad3
mohamedtalaat47 <mohamed_talaat_47@yahoo.com>
2018-10-21 22:27:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+new WOW().init();
+
+
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 5000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+  
+
+});
